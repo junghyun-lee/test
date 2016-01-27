57493c2695c9319ba4cfb25fccc9cd83bfedc21f
leejunghyun <mygumi@cizion.com>
2016-01-27 23:03:45 +0900

    express.Router 클래스

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function timeLog(req, res, next) {
+    console.log('Time: ', Date.now());
+    next();
+});
+// define the home page route
+router.use(function(req, res, next) {
+    console.log("호호호");
+    next();
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' , title2:'svsd'});
 });
 
 module.exports = router;
