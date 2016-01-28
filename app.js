9a8ffb96f72a385d5b43e41ba9c0c7c5dc4a6cfb
leejunghyun <mygumi@cizion.com>
2016-01-28 10:07:09 +0900

    next() next('route') 활용

--- app.js
+++ app.js
@@ -12,23 +12,22 @@ app.set('view engine', 'jade');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-//하나의 경로로 두개의 라우터 선언됨
-//요청 응답 주기를 종료하므로 두번째 라우터 절대 호출 안됨
-//next('route') 사용 시 미들웨어 스택의 나머지 미들웨어 함수를 건너뛰게됨
-//그리하여 두번째 라우터 호출됨
+
 app.get('/user/:id', function (req, res, next) {
-    console.log('ID:', req.params.id);
-    next('route');
+    // if the user ID is 0, skip to the next route
+    if (req.params.id == 0) next('route');
+    // otherwise pass the control to the next middleware function in this stack
+    else next(); //
 }, function (req, res, next) {
-    res.send('User Info');
+    // render a regular page
+    res.render('regular');
 });
 
-// handler for the /user/:id path, which prints the user ID
+// handler for the /user/:id path, which renders a special page
 app.get('/user/:id', function (req, res, next) {
-    console.log('야호');
-    res.end(req.params.id);
+    res.render('special');
 });
-////////////////////////////////////////////
+
 app.use('/', routes);
 app.use('/users', users);
 
